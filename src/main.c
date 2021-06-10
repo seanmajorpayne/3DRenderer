0d270923df5985f904ff05dbc2cf3f367f30b913
seanmajorpayne <seanmajorpayne@gmail.com>
2021-06-10 20:18:00 +0900

    adding color buffer & rendering

--- src/main.c
+++ src/main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
+bool is_running = false;
+
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
-bool is_running = false;
+
+uint32_t* color_buffer = NULL;
+SDL_Texture* color_buffer_texture = NULL;
+
+int window_width = 0;
+int window_height = 0;
 
 bool initialize_window(void) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -36,8 +44,21 @@ bool initialize_window(void) {
     return true;
 }
 
-void setup(void) {
-    // TODO
+bool setup(void) {
+    SDL_GetWindowSize(window, &window_width, &window_height);
+    color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
+    if (!color_buffer) {
+        puts("No memory.\n");
+        return false;
+    }
+    color_buffer_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        window_width,
+        window_height
+    );
+    return true;
 }
 
 void process_input(void) {
@@ -59,24 +80,54 @@ void update(void) {
     // TODO
 }
 
+void render_color_buffer(void) {
+    SDL_UpdateTexture(
+        color_buffer_texture,
+        NULL,
+        color_buffer,
+        window_width * sizeof(uint32_t)
+    );
+    SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
+void clear_color_buffer(uint32_t color) {
+    for (int x = 0; x < window_height * window_width; x++) {
+        color_buffer[x] = color;
+    }
+}
+
 void render(void) {
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_RenderClear(renderer);
 
+    render_color_buffer();
+    clear_color_buffer(0xFFFFFF00);
+
     SDL_RenderPresent(renderer);
+}
+
+void destroy_window(void) {
+    free(color_buffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
 
 int main(void) {
     /* TODO: Create SDL Window */
     is_running = initialize_window();
 
-    setup();
+    if(!setup()) {
+        return -1;
+    };
 
     while (is_running) {
         process_input();
         update();
         render();
     }
+
+    destroy_window();
 
     return 0;
 }
